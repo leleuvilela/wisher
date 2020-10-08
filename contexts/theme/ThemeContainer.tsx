1c6955b98d7ce0145d2b052de3a39d5f904c2283
Vinícius Vilela Nunes <vinicius.nunes@e-deploy.com.br>
2020-10-07 21:48:08 -0300

    change document and icon

--- contexts/theme/ThemeContainer.tsx
+++ contexts/theme/ThemeContainer.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 
 import { 
   ThemeProvider as ChakraThemeProvider, 
@@ -11,6 +12,11 @@ import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming'
 import theme from '../../styles/theme';
 
 const ThemeContainer: React.FC = ({ children }) => {
+  useEffect(() => {
+    ReactGA.initialize('UA-180073662-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+  
   return (
     <ChakraThemeProvider theme={theme}>
       <ColorModeProvider value="dark">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,13 @@ class MyDocument extends Document<DocumentProps> {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap" rel="stylesheet" />  
+          <title>wisher</title>
+          <meta name="Description" content="Faça um desejo, ele pode se realizar! Nesse site você coloca um desejo seu para público, fazendo assim com que alguma alma bondosa possa realiza-lo."></meta>
         </Head>
         <body>
           <Main />
           <NextScript />
+          <script data-ad-client="ca-pub-2661401025618183" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </body>
       </Html>
     );
